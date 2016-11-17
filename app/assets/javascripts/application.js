7c33b85330642944d44f725efbbf8422db6980cc
Rafa Ramirez <raffe90@gmail.com>
2016-11-17 03:36:59 +0100

    Adds invitations with minimal styles on form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).on('click', '.invite-user-trigger', function(e){
+  $('.invite-user-form-wrapper').toggleClass('hide')
+  $('.invite-user-form').toggleClass('hide')
+})
