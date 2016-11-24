99c2eb1e1dac86ecf66564bf77edba22fc60b61b
Rafa Ramírez <raffe90@gmail.com>
2016-11-24 20:04:34 +0100

    Adds Printer model API endpoints

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -1,18 +1,39 @@
 class PrintersController < ApplicationController
+  def index
+    @printers = Printer.all
+    respond_to do |format|
+      format.html {  }
+      format.json { render json: {printers: @printers} }
+    end
+  end
+
   def create
     @printer = Printer.create(printer_params)
-    respond_to do |format|
-      format.html { redirect_to user_path(current_user) }
-      format.json { render :json => @printer.to_json }
+    if @printer.valid?
+      respond_to do |format|
+        format.html { redirect_to user_path(current_user), notice: "Printer created." }
+        format.json { render json: { printer: @printer, :message => "Printer created.", ok: true, status: 200}, :status => :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to user_path(current_user), alert: "Unable to create printer." }
+        format.json { render json: { printer: @printer, errors: @printer.errors :message => "Unable to create printer.", ok: false, status: 422}, :status => :unprocessable_entity }
+      end
     end
   end
 
   def update
     @printer = Printer.find(params[:id])
-    @printer.update_attributes(printer_params)
-    respond_to do |format|
-      format.html { redirect_to user_path(current_user) }
-      format.json { render :json => @printer.to_json }
+    if @printer.update_attributes(printer_params)
+      respond_to do |format|
+        format.html { redirect_to user_path(current_user), notice: "Printer updated." }
+        format.json { render json: { printer: @printer, :message => "Printer updated.", ok: true, status: 200}, :status => :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to user_path(current_user), alert: "Unable to update printer." }
+        format.json { render json: { printer: @printer, errors: @printer.errors :message => "Unable to update printer.", ok: false, status: 422}, :status => :unprocessable_entity }
+      end
     end
   end
 
@@ -22,7 +43,7 @@ class PrintersController < ApplicationController
     flash[:notice] = "Printer deleted successfully"
     respond_to do |format|
       format.html { redirect_to user_path(current_user) }
-      format.json { render :json => @printer.to_json }
+      format.json { render json: {printer: @printer, :message => "Printer deleted.", ok: true, status: 200}, :status => :ok }
     end
   end
 
