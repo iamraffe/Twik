0334a2d872d477ac1a221d4200720fc86fcf44f8
Rafa Ramírez <raffe90@gmail.com>
2016-11-24 17:52:57 +0100

    Adds super reference on registration create

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { super }
+    end
   end
 end
