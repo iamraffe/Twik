a3d93c3fa402fce7bef6fe616c70fd090864b24a
Rafa Ramírez <raffe90@gmail.com>
2016-11-25 14:45:43 +0100

    Removes references to auth token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_filter :authenticate_user!, only: :index
-  skip_before_filter :authenticate_user_from_token!
+  # skip_before_filter :authenticate_user_from_token!
   # acts_as_token_authentication_handler_for User
   def dashboard
 
