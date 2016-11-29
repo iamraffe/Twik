d3a2f2e02c760a557952c63c21ef984f91c06e62
Rafa Ramírez <raffe90@gmail.com>
2016-11-30 00:00:51 +0100

    Removing traces of devise_auth_token gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,9 @@ class User < ActiveRecord::Base
   end
 
   def password_match?
-     self.errors[:password] << "can't be blank" if password.blank?
-     self.errors[:password_confirmation] << "can't be blank" if password_confirmation.blank?
-     self.errors[:password_confirmation] << "does not match password" if password != password_confirmation
+     self.errors[:password] << "Password can't be blank" if password.blank?
+     self.errors[:password_confirmation] << "Password confirmation can't be blank" if password_confirmation.blank?
+     self.errors[:password_confirmation] << "Passwords do not match" if password != password_confirmation
      password == password_confirmation && !password.blank?
   end
 
