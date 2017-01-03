336af4983854b182e37a62467f1859d1b6c16264
Rafa Ramirez <raffe90@gmail.com>
2017-01-03 20:15:58 +0100

    Adds style improvements to color component

--- client/src/components/menuWidget/toolPanel/ColorPanel.js
+++ client/src/components/menuWidget/toolPanel/ColorPanel.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import _ from 'lodash'
+import tinycolor from 'tinycolor2'
 
 import * as colorActions from '../../../actions/colorActions'
 
@@ -58,14 +59,15 @@ class ColorPanel extends React.Component{
   }
 
   borderCSS = (color) => {
-    let c = color.substring(1)
-    var rgb = parseInt(c, 16);   // convert rrggbb to decimal
-    var r = (rgb >> 16) & 0xff;  // extract red
-    var g = (rgb >>  8) & 0xff;  // extract green
-    var b = (rgb >>  0) & 0xff;  // extract blue
-    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+    // console.log(color)
+    // let c = color.substring(1)
+    // var rgb = parseInt(c, 16);   // convert rrggbb to decimal
+    // var r = (rgb >> 16) & 0xff;  // extract red
+    // var g = (rgb >>  8) & 0xff;  // extract green
+    // var b = (rgb >>  0) & 0xff;  // extract blue
+    // var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
 
-    if (luma > 40) {
+    if (tinycolor(color).isDark()) {
       return 'none'
     }
     else{
