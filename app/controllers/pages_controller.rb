3552aa976d8d53ae0d64fbbe46dea240b2749142
Rafa Ramirez <raffe90@gmail.com>
2017-01-12 02:49:45 +0100

    Adds dashboard non-fresh-user view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
   # skip_before_filter :authenticate_user_from_token!
   # acts_as_token_authentication_handler_for User
   def dashboard
-
+    @menus = Menu.all
   end
 end
