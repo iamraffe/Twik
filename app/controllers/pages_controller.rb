8c9cc03691185e9e6f6627bca631dfafe94d46ad
Rafa Ramirez <raffe90@gmail.com>
2017-01-13 02:09:29 +0100

    Adds dashboard grouping by template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   # skip_before_filter :authenticate_user_from_token!
   # acts_as_token_authentication_handler_for User
   def dashboard
-    @menus = Menu.all
+    @grouped_menus = Menu.all.order(:updated_at).group_by(&:template_id)
+    @templates = Template.find(@grouped_menus.keys)
   end
 end
