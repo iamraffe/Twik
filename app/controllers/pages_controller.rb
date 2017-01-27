b891d856eef4327f328bdf4dd72c3b8e4fb92d27
Rafa Ramírez <raffe90@gmail.com>
2017-01-27 23:43:55 +0100

    Sorts menu on dash by most recently updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_filter :authenticate_user!, only: :index
-  
+
   def dashboard
-    @menus = Menu.all.order(:updated_at)
+    @menus = Menu.all.order('updated_at desc')
   end
 end
