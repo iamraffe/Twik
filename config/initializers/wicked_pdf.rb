6eab336eebbb4acb9e76a04b260ddaab6b37e19b
Rafa Ramirez <raffe90@gmail.com>
2017-04-04 19:17:04 +0200

    Change dpi to 72 on wicked_pdf config

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,12 +2,12 @@ require 'os'
 
 if OS.mac?
   WickedPdf.config = {
-    dpi: 360,
+    dpi: 380,
     exe_path: '/usr/local/bin/wkhtmltopdf'
   }
 else
   WickedPdf.config = {
-    # dpi: 96,
+    dpi: 72,
     print_media_type: true,
     disable_smart_shrinking: true
   }
