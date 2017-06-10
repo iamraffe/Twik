8b3e283a7ee84691f7c3de856c7a2e5243dbdf98
Rafa Ramirez <raffe90@gmail.com>
2017-06-10 20:02:39 +0200

    Adds scrolling on sign up button landing (#44)

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -3,3 +3,23 @@ $('.current-account .dropdown').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
 });
+
+$(document).on('click', 'a.box-background', function(event){
+  // Add smooth scrolling to all links in navbar
+
+// Prevent default anchor click behavior
+event.preventDefault();
+
+// Store hash
+var hash = this.hash;
+
+// Using jQuery's animate() method to add smooth page scroll
+// The optional number (900) specifies the number of milliseconds it takes    to scroll to the specified area
+ $('html, body').animate({
+   scrollTop: $(hash).offset().top
+ }, 1600, 'easeInOutCubic', function(){
+
+  // Add hash (#) to URL when done scrolling (default click behavior)
+  // window.location.hash = hash;
+ });
+ })
